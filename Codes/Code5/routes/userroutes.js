672ac5e25e47257f201051f5358748cf47927540
amitsrivastava4all <amitsrivastava4all@gmail.com>
2018-07-21 17:43:07 +0530

    Code

---
+++ Codes/Code5/routes/userroutes.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const userRoute = express.Router();
+userRoute.post('/dologin',(request,response)=>{
+  var userid = request.body.userid;
+  var pwd = request.body.pwd;
+    if(userid == pwd){
+      response.send('Welcome User '+userid);
+    }
+    else{
+      response.send('Invalid Userid or Password');
+    }
+  });
+ userRoute.get('/questions',(request,response)=>{
+   var questions = {id:10, name:'JS Stands for',ans1:'JavaScript',ans2:'Java Snake',ans3:'Both',ans4:'None of These',score:5};
+   //var json = JSON.stringify(questions);
+   response.json(questions);
+ })
+module.exports = userRoute;
